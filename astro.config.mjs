768bf80fc527469f17cc35e7b6e492bf76e3f7d5
Lanips9872 <jchwh.github.io@gmail.com>
2026-08-17 02:04:24 +0900

    feature: profileにアイコン画像と簡単な自己紹介を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [expressiveCode()]
 });
