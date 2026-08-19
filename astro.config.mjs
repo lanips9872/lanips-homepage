9da4e4a86d81e5294921f20c87eb207ad93747eb
Lanips9872 <jchwh.github.io@gmail.com>
2026-08-20 02:14:03 +0900

    vercelのdeploy成功祈願でastro.config.mjsを変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
-import { defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
   },
 
   integrations: [expressiveCode()]
